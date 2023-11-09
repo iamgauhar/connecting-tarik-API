00fa85e420e0ea657865809669b2b2e26ee1723d
iamgauhar <iamgauhar@yahoo.com>
2023-11-09 21:47:43 +0530

    Redesigned Social Links

--- controllers/socialMedia.js
+++ controllers/socialMedia.js
@@ -1,23 +1,14 @@
 import catchAsyncError from "../middlewares/catchAsyncError.js";
-import { instagramModel, socialMediaModel, youtubeModel } from "../models/socialMediaLinks.js";
+import {youtubeModel } from "../models/socialMediaLinks.js";
 import ErrorHandler from "../utils/errorHandler.js";
 
 export const getYtLink = catchAsyncError(async (req, res, next) => {
-    const YTLink = await youtubeModel.find().limit(2)
+    const YTLink = await youtubeModel.find().limit(3)
     res.status(200).json({
         success: true,
         response: YTLink,
     })
 })
-export const getInstaLink = catchAsyncError(async (req, res, next) => {
-    const IGLink = await instagramModel.find().limit(1)
-    res.status(200).json({
-        success: true,
-        response: IGLink,
-    })
-})
-
-
 
 export const uploadLink = catchAsyncError(async (req, res, next) => {
     const { link } = req.body
@@ -36,15 +27,7 @@ export const uploadLink = catchAsyncError(async (req, res, next) => {
             success: true,
             message: 'Link uploaded successfully',
         });
-    }
-    else if (link.includes("www.instagram.com")) {
-        const response = new instagramModel(req.body)
-        response.save()
-        res.status(200).json({
-            success: true,
-            message: 'Link uploaded successfully',
-        });
-    } else {
+    }else {
         res.status(200).json({
             success: false,
             message: 'Please submit valid link',
@@ -54,38 +37,3 @@ export const uploadLink = catchAsyncError(async (req, res, next) => {
 
 })
 
-export const updateLinks = catchAsyncError(async (req, res, next) => {
-    const _id = "654200c9097687ed5c2a5c3b"
-    const { link } = req.body
-    if (!link) {
-        res.status(400).json({
-            success: false,
-            message: "Link required"
-        })
-    }
-
-    if (link.includes("www.youtube.com")) {
-        const response = await socialMediaModel.findByIdAndUpdate(_id, { youtube: link })
-        if (!response) {
-            return next(new ErrorHandler(404, 'No Link found'));
-        }
-        res.status(200).json({
-            success: true,
-            message: 'Link updated successfully',
-        });
-    } else if (link.includes("www.instagram.com")) {
-        const response = await socialMediaModel.findByIdAndUpdate(_id, { instagram: link })
-        if (!response) {
-            return next(new ErrorHandler(404, 'No Link found'));
-        }
-        res.status(200).json({
-            success: true,
-            message: 'Link updated successfully',
-        });
-    } else {
-        res.status(200).json({
-            success: false,
-            message: 'Please submit valid link',
-        });
-    }
-})
--- routes/socialMedia.js
+++ routes/socialMedia.js
@@ -1,11 +1,9 @@
 
 import { Router } from "express";
-import { getInstaLink, getYtLink, updateLinks, uploadLink } from "../controllers/socialMedia.js";
+import { getYtLink, uploadLink } from "../controllers/socialMedia.js";
 const socialRoute = Router()
 
 socialRoute.get("/youtube", getYtLink)
-socialRoute.get("/instagram", getInstaLink)
 socialRoute.post("/new", uploadLink)
-socialRoute.patch("/update", updateLinks)
 
 export default socialRoute;
